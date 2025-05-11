7e0dd99ebeaf82584a5c5b7e4899b8db9ab10fbd
Greedywebminer <168652146+Greedywebminer@users.noreply.github.com>
2025-05-10 22:56:07 -0800

    Delete setup directory

--- setup/main_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-
-bash setup_core.sh
-bash setup_ssh.sh
-bash setup_miner.sh
-bash setup_boot.sh
-bash adb_reconnect.sh
--- setup/setup_core.sh
+++
@@ -1,7 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-
-echo "[*] Updating and upgrading Termux packages..."
-yes | pkg update && yes | pkg upgrade
-
-echo "[*] Installing required packages..."
-yes | pkg install openssh libjansson wget nano android-tools jq -y
